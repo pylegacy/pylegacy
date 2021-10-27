a2498d711a32d3f81bbeac2a00b77b6d6d79a5f2
Víctor Molina García <molinav@users.noreply.github.com>
2021-10-27 12:13:57 +0200

    Add initial files

---
+++ setup.py
@@ -0,0 +1,91 @@
+#! /usr/bin/env python
+# -*- coding: utf8 -*-
+# flake8: noqa: E122
+"""pylegacy -- Backports for abandoned Python versions."""
+
+import io
+import os
+from setuptools import setup
+from setuptools import find_packages
+from src.pylegacy import __version__
+
+
+def get_content(name, splitlines=False):
+    """Return the file contents with project root as root folder."""
+
+    here = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(here, name)
+    with io.open(path, encoding="utf-8") as fd:
+        content = fd.read()
+    if splitlines:
+        content = [row for row in content.splitlines() if row]
+    return content
+
+
+setup(**{
+    "name":
+        "pylegacy",
+    "version":
+        __version__,
+    "license":
+        "Python Software Foundation License",
+    "description":
+        "Backports for abandoned Python versions",
+    "long_description":
+        get_content("README.md"),
+    "long_description_content_type":
+        "text/markdown",
+    "url":
+        "https://github.com/pylegacy/pylegacy",
+    "maintainer":
+        "Víctor Molina García",
+    "maintainer_email":
+        "molinav@users.noreply.github.com",
+    "classifiers": [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    "keywords": [
+        "compatibility",
+        "backports",
+        "legacy",
+    ],
+    "package_dir":
+        {"": "src"},
+    "packages":
+        find_packages(where="src"),
+    "python_requires":
+        ", ".join([
+            ">=2.6",
+            "!=3.0.*",
+            "!=3.1.*",
+            "<4",
+        ]),
+    "extras_require": {
+        "dev":
+            get_content("requirements-dev.txt", splitlines=True),
+        "doc":
+            get_content("requirements-doc.txt", splitlines=True),
+    },
+    "project_urls": {
+        "Bug Tracker":
+            "https://github.com/pylegacy/pylegacy/issues",
+        "Source":
+            "https://github.com/pylegacy/pylegacy",
+    },
+})
---
+++ src/pylegacy/__init__.py
@@ -0,0 +1,3 @@
+"""pylegacy -- Backports for abandoned Python versions."""
+
+__version__ = "0.0.0+dev"
