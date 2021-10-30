2febde7cdf936b36aa0aed6fa34028154815bd5c
Víctor Molina García <molinav@users.noreply.github.com>
2021-10-30 23:24:50 +0200

    Add basic test for ResourceWarning through exceptions

---
+++ test/pylegacy/test_pylegacy_exceptions.py
@@ -0,0 +1,37 @@
+"""Tests for :mod:`pylegacy.exceptions`."""
+# pylint: disable=import-error
+# flake8: noqa: F821
+from __future__ import print_function
+
+import sys
+import unittest2 as unittest
+
+
+class TestPyLegacyExceptions(unittest.TestCase):
+    """Unittest class for :mod:`pylegacy.exceptions`."""
+
+    def setUp(self):
+        """Define the test scope variables."""
+
+    @unittest.skipIf(sys.version_info[:2] >= (3, 2), reason="no backport")
+    def test_resourcewarning_missing(self):
+        """Test that :class:`ResourceWarning` does not exist."""
+
+        def test_callable():
+            """Helper function."""
+            return ResourceWarning  # pylint: disable=undefined-variable
+
+        self.assertRaises(NameError, test_callable)
+
+    def test_resourcewarning_available(self):
+        """Test that :class:`ResourceWarning` exists with :mod:`pylegacy`."""
+
+        import warnings
+        from pylegacy.exceptions import ResourceWarning
+
+        with self.assertWarns(ResourceWarning):
+            warnings.warn("this is a ResourceWarning message", ResourceWarning)
+
+
+if __name__ == "__main__":
+    unittest.main()
